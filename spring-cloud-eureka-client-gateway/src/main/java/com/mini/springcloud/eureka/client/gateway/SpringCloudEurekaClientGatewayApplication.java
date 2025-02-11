2bb77d7319a09d7487d0e966501e5756096ebdb4
Jong-Min <codejomo99@gmail.com>
2025-02-11 15:09:40 +0900

    init: client gateway

---
+++ spring-cloud-eureka-client-gateway/src/main/java/com/mini/springcloud/eureka/client/gateway/SpringCloudEurekaClientGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mini.springcloud.eureka.client.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientGatewayApplication.class, args);
+    }
+
+}
