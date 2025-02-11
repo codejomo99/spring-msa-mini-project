3ad3641a6f65dc131f39ecc6f519b3dcd99dcc0b
Jong-Min <codejomo99@gmail.com>
2025-02-11 21:20:42 +0900

    init: client-order init #7

---
+++ spring-cloud-eureka-client-order/src/main/java/com/example/springcloud/eureka/client/order/SpringCloudEurekaClientOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.eureka.client.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientOrderApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-eureka-client-order/src/test/java/com/example/springcloud/eureka/client/order/SpringCloudEurekaClientOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.eureka.client.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaClientOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
