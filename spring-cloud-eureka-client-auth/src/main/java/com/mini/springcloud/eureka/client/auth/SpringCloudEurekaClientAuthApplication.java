0e89e058330569d3b5faeea35a97c678e0966aee
Jong-Min <codejomo99@gmail.com>
2025-02-11 12:51:16 +0900

    init: clinet auth #1

---
+++ spring-cloud-eureka-client-auth/src/main/java/com/mini/springcloud/eureka/client/auth/SpringCloudEurekaClientAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mini.springcloud.eureka.client.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientAuthApplication.class, args);
+    }
+
+}
