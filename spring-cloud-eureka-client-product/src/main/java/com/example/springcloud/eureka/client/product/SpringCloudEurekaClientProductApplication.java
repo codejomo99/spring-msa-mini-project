39d2512705b831014efe65631200e483204b2f13
Jong-Min <codejomo99@gmail.com>
2025-02-11 16:20:59 +0900

    init: client product #5

---
+++ spring-cloud-eureka-client-product/src/main/java/com/example/springcloud/eureka/client/product/SpringCloudEurekaClientProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.eureka.client.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientProductApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-eureka-client-product/src/test/java/com/example/springcloud/eureka/client/product/SpringCloudEurekaClientProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.eureka.client.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaClientProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
